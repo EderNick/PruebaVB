380739013c63968af5702360e1a8d0b58b3a4445
Eder Figueroa <eder.figueroa@outlook.com>
2015-02-07 00:39:08 -0500

    Primer Commit

---
+++ HolaMundo.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmHolaMundo
+
+    Private Sub btnPulsa_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPulsa.Click
+        If txtNombre.Text <> "" Then
+            MsgBox("Hola Mundo, mi nombre es: " & txtNombre.Text, MsgBoxStyle.Exclamation, Me.Text)
+        Else
+            MsgBox("Ingresa un nombre primero ¬¬'", MsgBoxStyle.Exclamation, Me.Text)
+        End If
+    End Sub
+End Class
